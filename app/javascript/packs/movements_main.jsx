e3d695de694424bb886d050c7fed569295f6781a
ForeverYoung1208 <siafin2010@gmail.com>
2018-09-15 23:08:06 +0300

    a

---
+++ app/javascript/packs/movements_main.jsx
@@ -0,0 +1,18 @@
+console.log('Hello from movements_main.jsx ')
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+
+import Day from './movements_components/day'
+
+class MovementApp extends React.Component {
+	render(){
+		return(
+			<div>
+				<h1>MovementApp here </h1>
+			<div>
+		)
+	}
+}
