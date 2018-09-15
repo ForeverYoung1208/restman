715834f15819ec4462854dc0a5c41898a57f737b
ForeverYoung1208 <siafin2010@gmail.com>
2018-09-16 00:00:36 +0300

    a

--- app/javascript/packs/movements_main.jsx
+++ app/javascript/packs/movements_main.jsx
@@ -5,14 +5,27 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 
-import Day from './movements_components/day'
+// import Day from './movements_components/day'
 
 class MovementApp extends React.Component {
 	render(){
 		return(
 			<div>
 				<h1>MovementApp here </h1>
-			<div>
+			</div>
 		)
 	}
+}
+
+
+document.onreadystatechange = () => {
+  if (document.readyState === "interactive") {
+  	const app = document.getElementById('app-movements')
+  	if (app){
+  		ReactDOM.render(
+  			<MovementApp/>
+  			,app
+  		)
+  	}
+  }
 }
