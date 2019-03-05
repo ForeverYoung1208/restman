5ed31797668c9c8906d517ee3767cae43046eeed
ForeverYoung1208 <siafin2010@gmail.com>
2019-03-05 22:04:23 +0200

    a

--- app/controllers/movement_groups_controller.rb
+++ app/controllers/movement_groups_controller.rb
@@ -69,6 +69,6 @@ class MovementGroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movement_group_params
-      params.require(:movement_group).permit(:type, :name, :description)
+      params.require(:movement_group).permit(:type, :name, :description, :direction)
     end
 end
