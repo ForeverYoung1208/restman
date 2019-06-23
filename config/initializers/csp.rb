8bee115805173bc1b5c665448f232fbcb2a7ac6b
Igor <siafin2010@gmail.com>
2019-06-23 23:38:08 +0300

    a

---
+++ config/initializers/csp.rb
@@ -0,0 +1,11 @@
+Rails.application.config.content_security_policy do |p|
+  p.default_src :self, :https
+  p.script_src :self, :https
+  p.connect_src :self, 'ws://localhost:3035', 'ws://192.168.0.252:3035',
+  										 'localhost:3000', '192.168.0.252:3000', 'localhost:3035', '192.168.0.252:3035'
+  p.img_src :self, :https, :data
+  p.font_src :self, :https
+  p.base_uri :self, :https
+  p.style_src :self, :https, :unsafe_inline
+  p.form_action :self, :https
+end
