aa32384b6b7210be258ad69869c9b54124c03bca
Igor <siafin2010@gmail.com>
2019-06-25 18:04:30 +0300

    a

--- app/javascript/packs/Day/CompaniesMovements/BankRests/bankRests.jsx
+++ app/javascript/packs/Day/CompaniesMovements/BankRests/bankRests.jsx
@@ -5,60 +5,19 @@ function BankTableHead(props){
   return(
   	<thead>
   		<tr> 
+  			<th>Код компанії</th>
 	  		{	banks.map(bank => (
-  				<th key={bank}> {bank}
-  					<div className="row">
-  						<div className="col-md-4">UAH</div>
-  						<div className="col-md-4">USD</div>
-  						<div className="col-md-4">EUR</div>
-  					</div>
+  				<th key={bank}> 
+  				  <div className="bank-name"> {bank}</div>
+  					<div className="rest-val">UAH</div>
+						<div className="rest-val">USD</div>
+						<div className="rest-val">EUR</div>
   				</th>
 
   			))}  			
   		</tr>
   		
 
-  		TODO  !!!! UNDER CONSTRUCTION HERE
-
-  		<tr>
-	  		<th rowSpan="2" className="c1">Код компанії</th>
-	  		<th colSpan="3" className="clong">Вх. залишок</th>
-
-  			<th colSpan="3" className="clong">Отримано</th>
-
-				<th rowSpan="2" className="cshort">Примітки
-					
-
-				</th>
-
-  			<th colSpan="3" className="clong">Витрачено</th>
-
-	  		<th rowSpan="2" className="cshort">Примітки
-
-	  		</th>
-	  		<th colSpan="3" className="clong">Вих. залишок</th>
-
-	  	</tr>
-
-			<tr >
-
-				<th>UAH</th>
-				<th>USD</th>
-				<th>EUR</th>
-
-				<th>UAH</th>
-				<th>USD</th>
-				<th>EUR</th>
-
-				<th>UAH</th>
-				<th>USD</th>
-				<th>EUR</th>
-				
-				<th>UAH</th>
-				<th>USD</th>
-				<th>EUR</th>
-
-			</tr>
 
   	</thead>
   )
@@ -66,6 +25,8 @@ function BankTableHead(props){
 
 export function BankRests(props) {
 	return(	
-		<BankTableHead banks={props.banks}/>
+  	<table className="table movements-table rests-table">
+			<BankTableHead banks={props.banks}/>
+		</table>
 	)
 }
--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -1,8 +1,9 @@
 Rails.application.config.content_security_policy do |p|
   p.default_src :self, :https
   p.script_src :self, :https, :unsafe_eval
-  p.connect_src :self, 'ws://localhost:3035', 'ws://192.168.0.252:3035',
-  										 'localhost:3000', '192.168.0.252:3000', 'localhost:3035', '192.168.0.252:3035'
+  p.connect_src :self, 'ws://localhost:3035', 'localhost:3000', 'localhost:3035',
+  											'ws://192.168.0.252:3035', '192.168.0.252:3000', '192.168.0.252:3035',
+  										 'ws://192.168.99.51:3035', '192.168.99.51:3000','192.168.99.51:3035'
   p.img_src :self, :https, :data
   p.font_src :self, :https
   p.base_uri :self, :https
